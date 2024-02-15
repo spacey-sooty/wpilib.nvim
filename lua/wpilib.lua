3c034a98730849be4e2f717378e79ead2308bc4c
Isaac Turner <spacey_sooty@outlook.com>
2024-02-15 15:31:23 +0800

    add some more features

--- lua/wpilib.lua
+++ lua/wpilib.lua
@@ -1,7 +1,9 @@
-local wpilib = function(opts)
+local M = {}
+
+M.wpilib = function(opts)
     if opts.args == "RefreshCppLsp" then
         vim.cmd("!./gradlew generateCompileCommands")
-        vim.cmd("!mv build/TargetedCompileCommands/linuxx86-64debug/compile_commands.json compile_commands.json")
+        vim.cmd("!mv build/TargetedCompileCommands/linuxathenadebug/compile_commands.json compile_commands.json")
         vim.cmd.LspRestart()
     end
 
@@ -12,6 +14,26 @@ local wpilib = function(opts)
     if opts.args == "Deploy" then
         vim.cmd("!./gradlew deploy")
     end
+
+    if opts.args == "Simulate" then
+        if M.get_lang() == "cpp" then
+            vim.cmd("!./gradlew :simulateNative")
+        end
+
+        if M.get_lang() == "java" then
+            vim.cmd("!./gradlew :simulateJava")
+        end
+    end
+
+    if opts.args == "InstallLibrary" or opts.args "Vendordep" then
+            url = vim.fn.input("URL > ")
+            vim.cmd("!./gradlew vendordep " .. url)
+    end
 end
 
-vim.api.nvim_create_user_command('WPILib', wpilib, { nargs = 1 })
+M.get_lang = function()
+    local object = vim.api.json_decode(vim.cmd("!cat .wpilib/wpilib_preferences.json"))
+    return object.currentLanguage
+end
+
+vim.api.nvim_create_user_command('WPILib', M.wpilib, { nargs = 1, complete=arglist})
